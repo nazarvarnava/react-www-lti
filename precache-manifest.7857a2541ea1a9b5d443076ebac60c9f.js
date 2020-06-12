9826100be4cbcbfe3e38fcf936681b61ca69b6c4
NazarVarnava <nazar.varnava@gmail.com>
2020-06-12 19:53:49 +0300

    Updates

---
+++ precache-manifest.7857a2541ea1a9b5d443076ebac60c9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7976e2134e25bed99d2dc5bc58056451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66ce6003960360e6e923",
+    "url": "/static/css/main.497976e0.chunk.css"
+  },
+  {
+    "revision": "b873e97b0d1e66647d49",
+    "url": "/static/js/2.e0aa3846.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e0aa3846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ce6003960360e6e923",
+    "url": "/static/js/main.927ec8d9.chunk.js"
+  },
+  {
+    "revision": "c4491a011f663520cce3",
+    "url": "/static/js/runtime-main.ed59313e.js"
+  },
+  {
+    "revision": "4e44bfa5d983b74b8ed078d8245c29c0",
+    "url": "/static/media/home.4e44bfa5.jpg"
+  },
+  {
+    "revision": "0928c31be9d57f0bf8ddf5cbd84cae1b",
+    "url": "/static/media/logo.0928c31b.png"
+  },
+  {
+    "revision": "218400ef4e84efb203fcaa84baf92a79",
+    "url": "/static/media/man.218400ef.png"
+  }
+]);
